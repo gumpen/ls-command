616448a512c168c1ac196e3d144fe6760bdaeeda
gumpen <shuhei1993727@gmail.com>
2020-04-15 19:20:52 +0900

    add -l option

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/user"
+	"strconv"
 	"strings"
+	"syscall"
 )
 
 func main() {
 	showDotFiles := flag.Bool("a", false, "show dot files")
+	longStat := flag.Bool("l", false, "show long file status")
 	reverse := flag.Bool("r", false, "list in reverse order")
 	flag.Parse()
 
@@ -32,6 +36,38 @@ func main() {
 		if !*showDotFiles && strings.HasPrefix(file.Name(), ".") {
 			continue
 		}
-		fmt.Println(file.Name())
+		if *longStat {
+			permission := file.Mode().String()
+
+			nlink := uint64(0)
+			ownerName := ""
+			groupName := ""
+			if sys := file.Sys(); sys != nil {
+				if stat, ok := sys.(*syscall.Stat_t); ok {
+					nlink = uint64(stat.Nlink)
+
+					u, err := user.LookupId(fmt.Sprint(stat.Uid))
+					if err != nil {
+						panic(err)
+					}
+					ownerName = u.Username
+
+					g, err := user.LookupGroupId(fmt.Sprint(stat.Gid))
+					if err != nil {
+						panic(err)
+					}
+					groupName = g.Name
+				}
+			}
+
+			byteSize := file.Size()
+			timeStamp := file.ModTime().Format("Jan 2 15:04")
+
+			fmt.Println(permission + " " + strconv.Itoa(int(nlink)) + " " + ownerName + " " + groupName + " " + strconv.Itoa(int(byteSize)) + " " + timeStamp + " " + file.Name())
+
+		} else {
+			fmt.Println(file.Name())
+
+		}
 	}
 }
